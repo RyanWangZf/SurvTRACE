0d40f37f31f42054a5cb00d86c4140dda1b9e287
Zifeng <1399320648@qq.com>
2021-10-07 09:11:36 -0500

    Update config.py

--- survtrace/config.py
+++ survtrace/config.py
@@ -2,7 +2,7 @@ from easydict import EasyDict
 
 STConfig = EasyDict(
     {
-        'data': 'metabric',
+        'data': 'metabric', # dataset name, in 'metabric', 'support', or 'seer'
         'num_durations': 5, # num of discrete intervals for prediction, e.g., num_dur = 5 means the whole period is discretized to be 5 intervals
         'horizons': [.25, .5, .75], # the discrete intervals are cut at 0%, 25%, 50%, 75%, 100%
         'seed': 1234,
@@ -16,18 +16,18 @@ STConfig = EasyDict(
         'num_feature': 9, # num of covariates of patients, should be set during load_data
         'num_numerical_feature': 5, # num of numerical covariates of patients, should be set during load_data
         'num_categorical_feature': 4, # num of categorical covariates of patients, should be set during load_data
-        'out_feature':3, # 
+        'out_feature':3, # equals to the length of 'horizons', indicating the output dim of the logit layer of survtrace
         'num_event': 1, # only set when using SurvTraceMulti for competing risks
         'hidden_act': 'gelu',
         'attention_probs_dropout_prob': 0.1,
-        'max_position_embeddings': 512,
         'early_stop_patience': 5,
         'initializer_range': 0.001,
         'layer_norm_eps': 1e-12,
-        'chunk_size_feed_forward': 0,
-        'output_attentions': False,
-        'output_hidden_states': False,
-        'tie_word_embeddings': True,
-        'pruned_heads': {},
+        'max_position_embeddings': 512, # # no use
+        'chunk_size_feed_forward': 0, # no use
+        'output_attentions': False, # no use
+        'output_hidden_states': False, # no use 
+        'tie_word_embeddings': True, # no use
+        'pruned_heads': {}, # no use
     }
 )

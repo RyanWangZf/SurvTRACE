c423d4c3c1334257503c0e28bd0d91b3419b93fd
Zifeng <1399320648@qq.com>
2021-10-07 09:09:05 -0500

    Update config.py

--- survtrace/config.py
+++ survtrace/config.py
@@ -3,20 +3,20 @@ from easydict import EasyDict
 STConfig = EasyDict(
     {
         'data': 'metabric',
-        'num_durations': 5,
-        'horizons': [.25, .5, .75],
+        'num_durations': 5, # num of discrete intervals for prediction, e.g., num_dur = 5 means the whole period is discretized to be 5 intervals
+        'horizons': [.25, .5, .75], # the discrete intervals are cut at 0%, 25%, 50%, 75%, 100%
         'seed': 1234,
         'checkpoint': './checkpoints/survtrace.pt',
-        'vocab_size': 8,
-        'hidden_size': 16,
-        'intermediate_size': 64,
-        'num_hidden_layers': 3,
-        'num_attention_heads': 2,
+        'vocab_size': 8, # num of all possible values of categorical features
+        'hidden_size': 16, # embedding size
+        'intermediate_size': 64, # intermediate layer size in transformer layer
+        'num_hidden_layers': 3, # num of transformers
+        'num_attention_heads': 2, # num of attention heads in transformer layer
         'hidden_dropout_prob': 0.0,
-        'num_feature': 9,
-        'num_numerical_feature': 5,
-        'num_categorical_feature': 4,
-        'out_feature':3,
+        'num_feature': 9, # num of covariates of patients, should be set during load_data
+        'num_numerical_feature': 5, # num of numerical covariates of patients, should be set during load_data
+        'num_categorical_feature': 4, # num of categorical covariates of patients, should be set during load_data
+        'out_feature':3, # 
         'num_event': 1, # only set when using SurvTraceMulti for competing risks
         'hidden_act': 'gelu',
         'attention_probs_dropout_prob': 0.1,
